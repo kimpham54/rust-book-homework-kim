00106a38724a3f8809ede8db36e96dc791b21923
kimpham54 <kimpham54@gmail.com>
2022-10-23 18:24:23 +0200

    ch 4 ownership tests

--- ch4/ownership/src/main.rs
+++ ch4/ownership/src/main.rs
@@ -1,17 +1,25 @@
 fn main() {
-    let s1 = String::from("hello");
-    let s2 = s1;
+    let s = String::from("hello"); // s comes into scope
+    println!("{}", s); // s is valid
+    takes_ownership(s); // s's value moves into the function...
+                        // ... and so is no longer valid here
+                        // println!("{s}"); // error: value borrowed here after move
+    let x = 5; // x comes into scope
+    println!("{}", x); // x is valid
+    makes_copy(x); // x would move into the function,
+                   // but i32 is Copy, so it's okay to still
+                   // use x afterward
+    println!("{}", x); // x is valid
+} // Here, x goes out of scope, then s. But because s's value was moved, nothing
+  // special happens.
 
-    println!("{}, world!", s1);
+fn takes_ownership(some_string: String) {
+    // some_string comes into scope
+    println!("{}", some_string);
+} // Here, some_string goes out of scope and `drop` is called. The backing
+  // memory is freed.
 
-    // let s1 = String::from("hello");
-    // let s2 = s1.clone();
-
-    // println!("testing some {s1}{s2}");
-
-    // let mut x = 5;
-    // let y = x;
-    // println!("x = {}, y = {}", x, y);
-    // x = 6;
-    // println!("x = {}, y = {}", x, y);
-}
+fn makes_copy(some_integer: i32) {
+    // some_integer comes into scope
+    println!("{}", some_integer);
+} // Here, some_integer goes out of scope. Nothing special happens.
