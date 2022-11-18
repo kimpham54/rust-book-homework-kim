0f1dc335eb20015a72c426b6614acbd766b8ca87
kimpham54 <kimpham54@gmail.com>
2022-11-18 17:14:38 +0100

    done ch 12

--- ch12/minigrep/src/lib.rs
+++ ch12/minigrep/src/lib.rs
@@ -1,11 +1,12 @@
+use std::env;
 use std::error::Error;
 use std::fs;
-
 // cargo run -- frog poem.txt -> How public, like a frog
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -16,14 +17,26 @@ impl Config {
         let query = args[1].clone(); //frog, args[0] is the program name
         let file_path = args[2].clone(); //poem.txt
 
-        Ok(Config { query, file_path }) //create Config instance
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        }) //create Config instance
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{line}");
         // for each line in the results vector (the return of the search function) print the line
     }
@@ -41,17 +54,47 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    // $ IGNORE_CASE=lalala cargo run -- to poem.txt
+    //new query shadows the old query variable, creates a new String rather than a string slice (&str). somehow causes you to need &query to use it in contains
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            // uses &query here as the Pattern trait. why sometimes rust knows to assume an &, here it wants & to be explicit. also why can't it take ownership here of query?
+            results.push(line);
+        }
+    }
+    results
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three";
+Pick three.
+Duct tape.";
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
--- ch12/minigrep/src/main.rs
+++ ch12/minigrep/src/main.rs
@@ -10,18 +10,18 @@ fn main() {
     // create a vector, composed of strings from command line arguments, collect into the vector (need to collect because args is an iterator)
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
     // create a Config instance, populate it with values from the command line arguments
     // Err 'not enough arguments' would get passed here in else condition <3
-    // use unwrap or else, unwrap gets the value out of the Ok result, unwraps it and gives it to Config here
+    // use unwrap or else, unwrap gets the value out of the Ok result in build to make it accessible, unwraps it and gives it to Config here
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
+    eprintln!("Searching for {}", config.query);
+    eprintln!("In file {}", config.file_path);
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
     // we don't use unwrap here, just if let config::run Error because we don't need unwrapping to get the value from Ok or Error, Ok just returns () in run function
